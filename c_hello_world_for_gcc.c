736fe40d8cf92674caf6064833dbab1b9973d96b
Amaliya Romanova <1132226167@pfur.ru>
2023-03-09 13:03:53 +0300

    rewrite hello_world

--- c_hello_world_for_gcc.c
+++ c_hello_world_for_gcc.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main() 
+int main(int narg, char* arg[])                  // the first arg is how many arg heva main
 {
-    printf("Hello, world!");
+    printf("Hello, %s!\n", arg[1]);
+    
     getchar();
     return 0;
     
